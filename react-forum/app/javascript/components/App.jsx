776638a6b1e5337e3b422543b5c38ecb1ecdea88
Drustan <drustanyjt@gmail.com>
2023-01-23 08:29:31 +0800

    modified default session values

--- react-forum/app/javascript/components/App.jsx
+++ react-forum/app/javascript/components/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Routes from "../routes";
 import { ReactSession } from 'react-client-session';
 function App(props) {
-    //  ReactSession.set("user_name", "Anon");
-//    ReactSession.set("user_id", 1);
+    ReactSession.set("user_name", "Anon");
+    ReactSession.set("user_id", 1);
 
     return <>{Routes}</>
 }
